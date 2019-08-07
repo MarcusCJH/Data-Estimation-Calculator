dae4a8387cef0626349b32a7858422d068d51c13
MarcusCJH <marcuschanjh@gmail.com>
2019-08-07 17:13:28 +0800

    Validation for integer max value

--- Data-Estimation-Calculator/Main.cs
+++ Data-Estimation-Calculator/Main.cs
@@ -27,24 +27,35 @@ namespace Data_Estimation_Calculator
             decimal d;
             if (decimal.TryParse(tbxEstData.Text, out d) && decimal.TryParse(tbxPerMin.Text, out d)) 
             {
-                //valid
-                grpBxCalculation.Show();
-                DateTime d1 = DateTime.Now; //Getter DateTime of first submit click
-                int estData = Convert.ToInt32(tbxEstData.Text); // Getter for Estimation data
-                double perMin = Convert.ToDouble(tbxPerMin.Text); //Getter for Per Minute
-                double perHr = (estData / perMin) * 60; // Compute Logic for Per Hr
-                double perDay = perHr * 24; // Compute Logic for Per Day
-                double estDateTime = estData / perHr; // Compute Logic for Estimate of hr completion
-                var timeSpan = TimeSpan.FromHours(estDateTime);
-                lblPerHr.Text = perHr.ToString(); // Set Label perHr
-                lblPerDay.Text = perDay.ToString();// Set Label perDay
-                lblTimeNow.Text = d1.ToString("MM/dd/yyyy h:mm tt"); // set Label DateTime
-                lblTimeComplete.Text = d1.AddHours(estDateTime).ToString("MM/dd/yyyy h:mm tt") + " (" + timeSpan.Hours.ToString() + " Hours " + timeSpan.Minutes.ToString() + " Mins)"; //Set Label add Hours
+                if(d <= int.MaxValue)
+                {
+                    //valid
+                    grpBxCalculation.Show();
+                    DateTime d1 = DateTime.Now; //Getter DateTime of first submit click
+                    int estData = Convert.ToInt32(tbxEstData.Text); // Getter for Estimation data
+                    double perMin = Convert.ToDouble(tbxPerMin.Text); //Getter for Per Minute
+                    double perHr = (estData / perMin) * 60; // Compute Logic for Per Hr
+                    double perDay = perHr * 24; // Compute Logic for Per Day
+                    double estDateTime = estData / perHr; // Compute Logic for Estimate of hr completion
+                    var timeSpan = TimeSpan.FromHours(estDateTime);
+                    lblPerHr.Text = perHr.ToString(); // Set Label perHr
+                    lblPerDay.Text = perDay.ToString();// Set Label perDay
+                    lblTimeNow.Text = d1.ToString("MM/dd/yyyy h:mm tt"); // set Label DateTime
+                    lblTimeComplete.Text = d1.AddHours(estDateTime).ToString("MM/dd/yyyy h:mm tt") + " (" + timeSpan.Hours.ToString() + " Hours " + timeSpan.Minutes.ToString() + " Mins)"; //Set Label add Hours
+                }
+                else
+                {
+                    MessageBox.Show("Out of range");
+                    grpBxCalculation.Hide();
+                    return;
+                }
+                
             }
             else
             {
                 //invalid
                 MessageBox.Show("Please enter a valid number");
+                grpBxCalculation.Hide();
                 return;
             }
             
