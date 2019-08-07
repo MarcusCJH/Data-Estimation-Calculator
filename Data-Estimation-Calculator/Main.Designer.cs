990d47168758e152df79014971e10d1b7f26d869
MarcusCJH <marcuschanjh@gmail.com>
2019-08-07 14:54:02 +0800

    Logic computation

--- Data-Estimation-Calculator/Main.Designer.cs
+++ Data-Estimation-Calculator/Main.Designer.cs
@@ -28,11 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblTimeNow = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
             this.lblTimeComplete = new System.Windows.Forms.Label();
+            this.lblPerDay = new System.Windows.Forms.Label();
             this.lblPerHr = new System.Windows.Forms.Label();
-            this.lblPerHour = new System.Windows.Forms.Label();
             this.lbl5 = new System.Windows.Forms.Label();
             this.lbl3 = new System.Windows.Forms.Label();
             this.lbl2 = new System.Windows.Forms.Label();
@@ -41,9 +42,6 @@
             this.tbxEstData = new System.Windows.Forms.TextBox();
             this.tbxPerMin = new System.Windows.Forms.TextBox();
             this.btnSubmit = new System.Windows.Forms.Button();
-            this.timerNow = new System.Windows.Forms.Timer(this.components);
-            this.lblTimeNow = new System.Windows.Forms.Label();
-            this.lbl4 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -52,49 +50,64 @@
             this.groupBox1.Controls.Add(this.lblTimeNow);
             this.groupBox1.Controls.Add(this.lbl4);
             this.groupBox1.Controls.Add(this.lblTimeComplete);
+            this.groupBox1.Controls.Add(this.lblPerDay);
             this.groupBox1.Controls.Add(this.lblPerHr);
-            this.groupBox1.Controls.Add(this.lblPerHour);
             this.groupBox1.Controls.Add(this.lbl5);
             this.groupBox1.Controls.Add(this.lbl3);
             this.groupBox1.Controls.Add(this.lbl2);
             this.groupBox1.Location = new System.Drawing.Point(13, 84);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(399, 141);
+            this.groupBox1.Size = new System.Drawing.Size(399, 155);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Calculation";
             // 
+            // lblTimeNow
+            // 
+            this.lblTimeNow.AutoSize = true;
+            this.lblTimeNow.Location = new System.Drawing.Point(129, 95);
+            this.lblTimeNow.Name = "lblTimeNow";
+            this.lblTimeNow.Size = new System.Drawing.Size(0, 17);
+            this.lblTimeNow.TabIndex = 10;
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Location = new System.Drawing.Point(45, 95);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(74, 17);
+            this.lbl4.TabIndex = 9;
+            this.lbl4.Text = "Time Now:";
+            // 
             // lblTimeComplete
             // 
             this.lblTimeComplete.AutoSize = true;
-            this.lblTimeComplete.Location = new System.Drawing.Point(126, 121);
+            this.lblTimeComplete.Location = new System.Drawing.Point(129, 125);
             this.lblTimeComplete.Name = "lblTimeComplete";
             this.lblTimeComplete.Size = new System.Drawing.Size(46, 17);
             this.lblTimeComplete.TabIndex = 8;
             this.lblTimeComplete.Text = "label8";
             // 
+            // lblPerDay
+            // 
+            this.lblPerDay.AutoSize = true;
+            this.lblPerDay.Location = new System.Drawing.Point(129, 64);
+            this.lblPerDay.Name = "lblPerDay";
+            this.lblPerDay.Size = new System.Drawing.Size(0, 17);
+            this.lblPerDay.TabIndex = 7;
+            // 
             // lblPerHr
             // 
             this.lblPerHr.AutoSize = true;
-            this.lblPerHr.Location = new System.Drawing.Point(126, 64);
+            this.lblPerHr.Location = new System.Drawing.Point(129, 31);
             this.lblPerHr.Name = "lblPerHr";
-            this.lblPerHr.Size = new System.Drawing.Size(46, 17);
-            this.lblPerHr.TabIndex = 7;
-            this.lblPerHr.Text = "label7";
-            // 
-            // lblPerHour
-            // 
-            this.lblPerHour.AutoSize = true;
-            this.lblPerHour.Location = new System.Drawing.Point(126, 31);
-            this.lblPerHour.Name = "lblPerHour";
-            this.lblPerHour.Size = new System.Drawing.Size(46, 17);
-            this.lblPerHour.TabIndex = 6;
-            this.lblPerHour.Text = "label6";
+            this.lblPerHr.Size = new System.Drawing.Size(0, 17);
+            this.lblPerHr.TabIndex = 6;
             // 
             // lbl5
             // 
             this.lbl5.AutoSize = true;
-            this.lbl5.Location = new System.Drawing.Point(6, 121);
+            this.lbl5.Location = new System.Drawing.Point(2, 125);
             this.lbl5.Name = "lbl5";
             this.lbl5.Size = new System.Drawing.Size(117, 17);
             this.lbl5.TabIndex = 5;
@@ -103,7 +116,7 @@
             // lbl3
             // 
             this.lbl3.AutoSize = true;
-            this.lbl3.Location = new System.Drawing.Point(60, 64);
+            this.lbl3.Location = new System.Drawing.Point(56, 64);
             this.lbl3.Name = "lbl3";
             this.lbl3.Size = new System.Drawing.Size(63, 17);
             this.lbl3.TabIndex = 1;
@@ -158,35 +171,13 @@
             this.btnSubmit.TabIndex = 5;
             this.btnSubmit.Text = "Submit";
             this.btnSubmit.UseVisualStyleBackColor = true;
-            // 
-            // timerNow
-            // 
-            this.timerNow.Interval = 1000;
-            this.timerNow.Tick += new System.EventHandler(this.TimerNow_Tick);
-            // 
-            // lblTimeNow
-            // 
-            this.lblTimeNow.AutoSize = true;
-            this.lblTimeNow.Location = new System.Drawing.Point(126, 95);
-            this.lblTimeNow.Name = "lblTimeNow";
-            this.lblTimeNow.Size = new System.Drawing.Size(31, 17);
-            this.lblTimeNow.TabIndex = 10;
-            this.lblTimeNow.Text = "test";
-            // 
-            // lbl4
-            // 
-            this.lbl4.AutoSize = true;
-            this.lbl4.Location = new System.Drawing.Point(49, 95);
-            this.lbl4.Name = "lbl4";
-            this.lbl4.Size = new System.Drawing.Size(74, 17);
-            this.lbl4.TabIndex = 9;
-            this.lbl4.Text = "Time Now:";
+            this.btnSubmit.Click += new System.EventHandler(this.BtnSubmit_Click);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(424, 237);
+            this.ClientSize = new System.Drawing.Size(425, 246);
             this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.tbxPerMin);
             this.Controls.Add(this.tbxEstData);
@@ -206,8 +197,8 @@
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label lblTimeComplete;
+        private System.Windows.Forms.Label lblPerDay;
         private System.Windows.Forms.Label lblPerHr;
-        private System.Windows.Forms.Label lblPerHour;
         private System.Windows.Forms.Label lbl5;
         private System.Windows.Forms.Label lbl3;
         private System.Windows.Forms.Label lbl2;
@@ -218,7 +209,6 @@
         private System.Windows.Forms.Button btnSubmit;
         private System.Windows.Forms.Label lblTimeNow;
         private System.Windows.Forms.Label lbl4;
-        private System.Windows.Forms.Timer timerNow;
     }
 }
 
--- Data-Estimation-Calculator/Main.cs
+++ Data-Estimation-Calculator/Main.cs
@@ -17,9 +17,25 @@ namespace Data_Estimation_Calculator
             InitializeComponent();
         }
 
-        private void TimerNow_Tick(object sender, EventArgs e)
+        
+
+        private void BtnSubmit_Click(object sender, EventArgs e)
         {
-            lblTimeNow.Text = DateTime.Now.ToLongTimeString();
+
+           
+           
+            
+            DateTime d1 = DateTime.Now; //Getter DateTime of first submit click
+            int estData = Convert.ToInt32(tbxEstData.Text); // Getter for Estimation data
+            double perMin = Convert.ToDouble(tbxPerMin.Text); //Getter for Per Minute
+            double perHr = (estData / perMin) * 60; // Compute Logic for Per Hr
+            double perDay = perHr * 24; // Compute Logic for Per Day
+            double estDateTime = estData/perHr; // Compute Logic for Estimate of hr completion
+            var timeSpan = TimeSpan.FromHours(estDateTime);
+            lblPerHr.Text = perHr.ToString(); // Set Label perHr
+            lblPerDay.Text = perDay.ToString();// Set Label perDay
+            lblTimeNow.Text = d1.ToString("MM/dd/yyyy h:mm tt"); // set Label DateTime
+            lblTimeComplete.Text = d1.AddHours(estDateTime).ToString("MM/dd/yyyy h:mm tt") +" ("+timeSpan.Hours.ToString() +" Hours " +timeSpan.Minutes.ToString() +" Mins)"; //Set Label add Hours
         }
     }
 }
